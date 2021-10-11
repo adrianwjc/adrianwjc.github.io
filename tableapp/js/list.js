98aba15d18a8d2164506b02025fe041a27ed9d0e
Adrian Wong <adrianwjc@hotmail.com>
2021-10-11 20:08:24 +0800

    aaa

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -14,26 +14,26 @@ function GetBooking() {
             let bookingNameList = document.getElementById("bookingNameList")
 
             //delete all rows in the table
-            for (let k = bookingNamelist.rows.length - 1; k > 0; k--){
+            for (let k = bookingNamelist.rows.length - 1; k > 0; k--) {
                 bookingNameList.deleteRow(k)
             }
 
 
             //load all rows from Sheety API
-for(let i = 0; i < json.bookings.length; i++){
-    let gName = json.bookings[i].name;
-    let gEmail = json.bookings[i].email;
-    let gPax = json.bookings[i].pax;
-    let gRemarks = json.bookings[i].remarks;
-    let gId = json.bookings[i].id;
+            for (let i = 0; i < json.bookings.length; i++) {
+                let gName = json.bookings[i].name;
+                let gEmail = json.bookings[i].email;
+                let gPax = json.bookings[i].pax;
+                let gRemarks = json.bookings[i].remarks;
+                let gId = json.bookings[i].id;
 
-let row = bookingNameList.insertRow(bookingNameList.rows.length)
-row.insertCell(0).innerHTML = gId
-row.insertCell(1).innerHTML = gName
-row.insertCell(2).innerHTML = gEmail
-row.insertCell(3).innerHTML = gPax
-row.insertCell(4).innerHTML = gRemarks
-row.insertCell(5).innerHTML = ""
-}
+                let row = bookingNameList.insertRow(bookingNameList.rows.length)
+                row.insertCell(0).innerHTML = gId
+                row.insertCell(1).innerHTML = gName
+                row.insertCell(2).innerHTML = gEmail
+                row.insertCell(3).innerHTML = gPax
+                row.insertCell(4).innerHTML = gRemarks
+                row.insertCell(5).innerHTML = ""
+            }
         });
 }
