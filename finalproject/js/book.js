4ab2e00dd11c4427207e2c621b4f40326e3293e4
Adrian Wong <adrianwjc@hotmail.com>
2021-10-31 15:42:18 +0800

    aaa

--- finalproject/js/book.js
+++ finalproject/js/book.js
@@ -15,11 +15,11 @@ bookNowBtn.addEventListener("click",function(){
     let userRemarks  = document.getElementById("userRemarks")
     let userRemarksVal = userRemarks.value
 
-    preOrderNow(userNameVal, userEmailVal,userVariantVal,userQuantityVal,userRemarksVal)
+    preorderNow(userNameVal, userEmailVal,userVariantVal,userQuantityVal,userRemarksVal)
 })
 
 
-function preOrderNow(userName, userEmail, userVariant, userQuantity,userRemarks){
+function preorderNow(userName, userEmail, userVariant, userQuantity,userRemarks){
     let url = 'https://api.sheety.co/3916d578b401ffa66a19148295d570ad/finalProject/bookings';
     let body = {
         booking: {
